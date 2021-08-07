b2ebe18aff0bd6ce9726d9448be333151255cc72
Roman Romashcko <roman.romashcko@yandex.ru>
2021-08-08 01:13:38 +0300

    Export calc context and strategies

---
+++ src/calculation/index.ts
@@ -0,0 +1,11 @@
+import calculationContext from './context';
+import meanCalcStrategy from './strategy/meanStrategy';
+import sumCalcStrategy from './strategy/sumStrategy';
+import maxCalcStrategy from './strategy/maxStrategy';
+
+export {
+    calculationContext,
+    meanCalcStrategy,
+    sumCalcStrategy,
+    maxCalcStrategy
+}
